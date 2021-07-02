a2c6034e341ca677f072fc745f55e8430660e0b7
Kevin Decherf <kevin@kdecherf.com>
2021-07-03 01:04:46 +0200

    tests: replace ru url used for testLanguageValidation
    Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

--- tests/Wallabag/CoreBundle/Controller/EntryControllerTest.php
+++ tests/Wallabag/CoreBundle/Controller/EntryControllerTest.php
@@ -1387,7 +1387,7 @@ class EntryControllerTest extends WallabagCoreTestCase
     {
         return [
             'ru' => [
-                'https://www.vtimes.io/2020/12/14/ubiistvo-kotorogo-ne-bilo-a1981',
+                'https://ru.wikipedia.org/wiki/%D0%9E%D1%82%D0%BA%D1%80%D1%8B%D1%82%D0%BE%D0%B5_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%BD%D0%BE%D0%B5_%D0%BE%D0%B1%D0%B5%D1%81%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B5',
                 'ru',
             ],
             'fr' => [
